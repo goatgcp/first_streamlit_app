3b824abf878c6bb3f7c6fbaf3b29081836a1cabe
goatgcp <104343117+goatgcp@users.noreply.github.com>
2022-04-29 18:36:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import pandas
 import requests
 import snowflake.connector
 
-my_conn = snowflake.connector.connect(**streamlit.secrets["Snowflake"])
-my_cur = my_conn.cursor()
-my_cur.execute("Select current_user(), current_account(), current_region()")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text('Gday')
+streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
            
 streamlit.title('My Parents NEW Healthy Diner')
